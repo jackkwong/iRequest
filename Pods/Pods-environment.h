f91ca86bf6f6acb874a7ae953f476917160b8194
admin <admin@MacMini.local>
2014-07-21 19:03:58 +0800

    Added Cocoapods dependency

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,80 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ChuzzleKit
+#define COCOAPODS_POD_AVAILABLE_ChuzzleKit
+#define COCOAPODS_VERSION_MAJOR_ChuzzleKit 1
+#define COCOAPODS_VERSION_MINOR_ChuzzleKit 0
+#define COCOAPODS_VERSION_PATCH_ChuzzleKit 3
+
+// OMGHTTPURLRQ
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ 1
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ 4
+
+// OMGHTTPURLRQ/RQ
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ_RQ
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ_RQ 1
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ_RQ 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ_RQ 4
+
+// OMGHTTPURLRQ/UserAgent
+#define COCOAPODS_POD_AVAILABLE_OMGHTTPURLRQ_UserAgent
+#define COCOAPODS_VERSION_MAJOR_OMGHTTPURLRQ_UserAgent 1
+#define COCOAPODS_VERSION_MINOR_OMGHTTPURLRQ_UserAgent 1
+#define COCOAPODS_VERSION_PATCH_OMGHTTPURLRQ_UserAgent 4
+
+// PromiseKit
+#define COCOAPODS_POD_AVAILABLE_PromiseKit
+#define COCOAPODS_VERSION_MAJOR_PromiseKit 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit 11
+
+// PromiseKit/Foundation
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_Foundation
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_Foundation 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit_Foundation 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit_Foundation 11
+
+// PromiseKit/UIKit
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_UIKit
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_UIKit 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit_UIKit 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit_UIKit 11
+
+// PromiseKit/base
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_base
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_base 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit_base 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit_base 11
+
+// PromiseKit/defaults
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_defaults
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_defaults 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit_defaults 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit_defaults 11
+
+// PromiseKit/private
+#define COCOAPODS_POD_AVAILABLE_PromiseKit_private
+#define COCOAPODS_VERSION_MAJOR_PromiseKit_private 0
+#define COCOAPODS_VERSION_MINOR_PromiseKit_private 9
+#define COCOAPODS_VERSION_PATCH_PromiseKit_private 11
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 0
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 0
+
